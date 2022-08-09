fa861e05572d835fa366848a4be2f81e6260bc72
Igor Bihanskyi <IgorBihanskyi@gmail.com>
2022-08-09 15:21:31 +0300

    first commit

---
+++ RobotName/RobotNameGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace RobotName;
+
+public class RobotNameGenerator
+{
+    private static readonly HashSet<string> UsedNames = new();
+
+    public string GenerateName()
+    {
+        string NewName()
+        {
+            string guid = Guid.NewGuid().ToString();
+            return string
+                .Join("", guid.Where(x => Char.IsLetter(x)).Take(2).Concat(guid.Where(x => Char.IsDigit(x)).Take(3)))
+                .ToUpper();
+        }
+        while (true)
+        {
+            var name = NewName();
+            if (UsedNames.Add(name))
+            {
+                return name;
+            }
+        }
+    }
+}
